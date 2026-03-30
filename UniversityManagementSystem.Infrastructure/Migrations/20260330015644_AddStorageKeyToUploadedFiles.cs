7295e92732ecfb5ac83458feb89d1a554ac94cdf
Mohamed Mahmoud <mohamedmahmoudshaban31@gmail.com>
2026-03-30 03:57:14 +0200

    add StorageKey column to UploadedFiles

---
+++ UniversityManagementSystem.Infrastructure/Migrations/20260330015644_AddStorageKeyToUploadedFiles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStorageKeyToUploadedFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
